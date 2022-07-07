db58b492634254a12d39349b7826d332cc8e019b
Misha <Mishatrof10@mail.ru>
2022-07-07 19:14:30 +0300

    Add lobby

---
+++ Assets/Scripts/Server/ConnectToServer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+namespace Project
+{
+    public class ConnectToServer : MonoBehaviourPunCallbacks
+    {
+        public TMP_InputField NicknameField;
+        public void OnClickConnect()
+        {
+            if (NicknameField.text.Length <= 4) return;
+            
+            PhotonNetwork.NickName = NicknameField.text;
+            Debug.Log("Connecting");
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            SceneManager.LoadScene("Lobby");
+        }
+    }
+}
