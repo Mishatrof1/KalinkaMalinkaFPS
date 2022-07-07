3f22c9a72f041c85faff1a62cb4506c9a5e75a49
rus34legenda <rus34honor7x@gmail.com>
2022-07-08 01:55:59 +0300

    SHOOTTT
    SHOOTTT

---
+++ Assets/Scripts/Event Bus/Event Listener/EventBusExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Project
+{
+    public static partial class EventBusExtensions
+    {
+        /// <summary>
+        /// Подписывает слушателя на событие.
+        /// </summary>
+        /// <typeparam name="T">Тип события.</typeparam>
+        /// <param name="listenerAction">Метод, вызываемый при событии.</param>
+        /// <param name="isRemovable">Может ли слушатель отписаться от события досрочно.</param>
+        /// <returns>Экземпляр слушателя события.</returns>
+        public static EventListener<T> AddListener<T>(this EventBus eventBus, Action<T> listenerAction, bool isRemovable = true) where T : struct
+        {
+            EventListener<T> listener = new EventListener<T>(listenerAction, isRemovable);
+
+            eventBus.RegisterListenerEvent(typeof(T), listener);
+
+            return listener;
+        }
+
+        /// <summary>
+        /// Отписывает слушателя от события.
+        /// </summary>
+        /// <typeparam name="T">Тип события.</typeparam>
+        /// <param name="listener">Экземпляр слушателя события.</param>
+        public static void RemoveListener<T>(this EventBus eventBus, EventListener<T> listener) where T : struct
+        {
+            eventBus.UnregisterListenerEvent(typeof(T), listener.Hash);
+        }
+
+        /// <summary>
+        /// Отправляет событие.
+        /// </summary>
+        /// <typeparam name="T">Тип события.</typeparam>
+        /// <param name="message">Экземпляр события.</param>
+        public static void Send<T>(this EventBus eventBus, T message) where T : struct
+        {
+            eventBus.PostEvent(message);
+        }
+
+        /// <summary>
+        /// Очищает всех слушателей, отмеченных флагом 'isRemovable'.
+        /// </summary>
+        public static void CleanRemovableListeners(this EventBus eventBus)
+        {
+            eventBus.CleanRemovableListenerEvent();
+        }
+    }
+}
--- Assets/Scripts/Health System/HealthObject.cs
+++ Assets/Scripts/Health System/HealthObject.cs
@@ -11,16 +11,37 @@ namespace Project
         public void Damage(int damage)
         {
             Health -= damage;
+
+            if (Health < 0)
+                Health = 0;
+
+            CheckDeath();
         }
 
         public void Fill(int health)
         {
+            int newHealth = Health + health;
 
+            if (newHealth > _startHealth)
+                newHealth = _startHealth;
+
+            Health = newHealth;
         }
 
-        private void Start()
+        private void Awake()
         {
             _startHealth = Health;
+        }
+
+        private void CheckDeath()
+        {
+            if (Health == 0)
+                Death();
+        }
+
+        private void Death()
+        {
+            Destroy(gameObject);
         }
     }
 }
